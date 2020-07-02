2096878ed73452a42f191e2642f518dcba8bcdca
guy.peled <guy.peled@unity3d.com>
2020-07-02 23:53:08 +0200

    Remove non required code

--- processor/processor.go
+++ processor/processor.go
@@ -3,25 +3,21 @@ package processor
 import (
 	"cloud.google.com/go/bigquery"
 	"database/sql/driver"
-	"github.com/sirupsen/logrus"
 )
 
-type DatasetProvider interface {
+type Provider interface {
 	GetDataset() *bigquery.Dataset
 }
 
-func Exec(provider DatasetProvider, query string, args []driver.Value) (driver.Result, error) {
+func Exec(provider Provider, query string, args []driver.Value) (driver.Result, error) {
 	return nil, nil
 }
 
-func Query(provider DatasetProvider, query string, args []driver.Value) (driver.Rows, error) {
+func Query(provider Provider, query string, args []driver.Value) (driver.Rows, error) {
 
 	handler := processors[query]
 	if handler != nil {
-
-		logrus.Debugf("processor:%s", query)
-
-		return handler(provider.GetDataset(), args)
+		return handler(provider, args)
 	}
 
 	return nil, nil
--- processor/processors.go
+++ processor/processors.go
@@ -1,14 +1,15 @@
 package processor
 
 import (
-	"cloud.google.com/go/bigquery"
 	"database/sql/driver"
 )
 
-type processorHandler func(*bigquery.Dataset, []driver.Value) (driver.Rows, error)
+type processorHandler func(Provider, []driver.Value) (driver.Rows, error)
 
 var processors = map[string]processorHandler{
-	hasTable: func(dataset *bigquery.Dataset, args []driver.Value) (driver.Rows, error) {
-		return createValueRows(dataset.Table(args[0].(string)) != nil), nil
-	},
+	hasTable: processHasTable,
+}
+
+func processHasTable(provider Provider, args []driver.Value) (driver.Rows, error) {
+	return createValueRows(provider.GetDataset().Table(args[0].(string)) != nil), nil
 }
